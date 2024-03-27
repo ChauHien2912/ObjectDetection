69f11aa76601221767454e4dd428e4e8fec5d9e4
mtoan87 <mt.lengendary@gmail.com>
2024-03-27 21:47:34 +0700

    Update LoadDectectionData

--- WPF_MachineService/MainWindow.xaml.cs
+++ WPF_MachineService/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_MachineService.Models;
 
 namespace WPF_MachineService
 {
@@ -16,9 +19,41 @@ namespace WPF_MachineService
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DetectionObjectContext context;
+        Scanning messageBox = new Scanning();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private async void LoadDetectionData(string json)
+        {
+            Window window = new Window
+            {
+                Content = messageBox,
+                SizeToContent = SizeToContent.WidthAndHeight,
+                WindowStyle = WindowStyle.None,
+                ResizeMode = ResizeMode.NoResize,
+                WindowStartupLocation = WindowStartupLocation.CenterScreen,
+                Title = "Scanning"
+            };
+            window.Show();
+            await Task.Delay(2000);
+            List<Product> productsToDisplay = new List<Product>();          
+            JObject jsonObject = JObject.Parse(json);
+            JArray predictions = (JArray)jsonObject["predictions"];
+            foreach (var prediction in predictions)
+            {
+                string className = (string)prediction["class"];             
+                var productsInDB = context.Products.Where(p => p.ProName == className).ToList();             
+                if (productsInDB == null)
+                {               
+                    MessageBox.Show("No product found in the database matching the class: " + className);
+                }
+               
+            }           
+            lvListView.ItemsSource = productsToDisplay;
+            lvListView.DataContext = this;                      
+            window.Close();
         }
     }
 }
